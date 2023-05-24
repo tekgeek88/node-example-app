330c0b2a0756569cfe264721086bf851428c2eb7
Carl Argabright <carl.argabright@gmail.com>
2023-05-24 02:51:21 -0700

    Initial commit

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example') {
+    scm {
+        git('https://github.com/tekgeek88/node-example-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('carl.argabright@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/2 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
