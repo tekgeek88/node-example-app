dda37db3422101ecbbb3821d805231c093d1ea6d
Carl Argabright <carl.argabright@gmail.com>
2023-05-24 03:03:12 -0700

    Updated NodeJS instance name

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -10,7 +10,7 @@ job('NodeJS example') {
         scm('H/2 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('NodeJS') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
